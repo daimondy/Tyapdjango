89c94c5d0122c435a4a0e7c1aeaf68491f780737
Elchi DIana <47711461+daimondy@users.noreply.github.com>
2019-04-14 19:35:07 +0300

    pew

--- lab2/firstwebpage/firstwebpage/urls.py
+++ lab2/firstwebpage/firstwebpage/urls.py
@@ -2,10 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from pages import views
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('hello/', views.home, name='home'),
 ]
-
